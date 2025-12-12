6d0ef0d16d773f8b19f604da796b2bd258128233
mf7 <mf7@IOT.myguest.virtualbox.org>
2025-12-12 18:12:47 +0100

    p1

--- p1/scripts/init_server.sh
+++ p1/scripts/init_server.sh
@@ -1,5 +1,13 @@
+sudo apt update -y
+sudo apt install curl -y
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --flannel-iface=eth1" sh -
-
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
-
 echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+
+mkdir -p ~/.kube
+
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+
+sudo chown $(id -u):$(id -g) ~/.kube/config
+
+chmod 600 ~/.kube/config
--- p1/scripts/init_worker.sh
+++ p1/scripts/init_worker.sh
@@ -1,5 +1,19 @@
-sudo apt update
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111 --flannel-iface=eth1" sh -
-kubectl get node
+#!/bin/bash
+sudo apt update -y && apt install curl tree -y
 
+while ! tree /vagrant | grep -q node-token; do
+    echo "Waiting for Server building... ⏳"
+    sleep 1
+done
+
+# mkdir -p ~/.ssh
+# sudo cp /vagrant/.vagrant/machines/rwassimS/libvirt/private_key ~/.ssh/id_rsa_master
+# chmod 600 ~/.ssh/id_rsa_master
+# ssh-keyscan -H 192.168.56.110 >> ~/.ssh/known_hosts
+
+echo "WORKER APT UPDATED 📚"
 TOKEN=$(cat /vagrant/node-token)
+echo "WORKER TOKEN CREATED 🪙"
+
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111 --flannel-iface=eth1" sh -
+echo "WORKER READY 🟩"
