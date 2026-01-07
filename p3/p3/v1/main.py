0309a9b78178e8e2dd9bad8eb44be6f8033b487c
mfernand <mfernand@Perso.Perso.org>
2026-01-07 15:21:01 +0100

    wip

--- p3/p3/v1/main.py
+++ p3/p3/v1/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>BIENVENUE</h1>"
+    return "<h1>WELCOME V1</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000)
--- p3/p3/v2/main.py
+++ p3/p3/v2/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>WELCOME</h1>"
+    return "<h1>BIENVENUE V2</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000)
