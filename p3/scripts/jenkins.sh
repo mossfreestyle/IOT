1de57c5385f0fbd616df7a9e014bc755991b7536
mfernand <mfernand@Perso.Perso.org>
2026-01-09 00:29:15 +0100

    need to check how argocd and jenkins functions with the gui, and after need to implement both cli and check about some missing depedencies"

--- p3/scripts/jenkins.sh
+++ p3/scripts/jenkins.sh
@@ -1,5 +1,12 @@
-docker run --name jenkins -d -p 8081:8080 -p 50000:50000 jenkins/jenkins:lts > /dev/null
+docker run -d \
+  --name jenkins \
+  -p 8081:8080 \
+  -p 50000:50000 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v jenkins_home:/var/jenkins_home \
+  jenkins/jenkins:lts > /dev/null
 
+mkdir -p ../credentials
 "" > ../credentials/jenkins_pass.txt 2>/dev/null
 sleep 5
 docker cp jenkins:/var/jenkins_home/secrets/initialAdminPassword ../credentials/jenkins_pass.txt
